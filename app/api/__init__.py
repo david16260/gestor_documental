7a2f1a06f20961c2c42b8278aa4cf275072fe07d
mauricio-xf <mauricio1234madera@gmail.com>
2025-11-14 07:40:13 -0500

    Update __init__.py

---
+++ app/api/__init__.py
@@ -0,0 +1,15 @@
+from . import auth
+from . import documentos
+from . import documentos_versiones
+from . import fuid
+from . import documentos_url
+from . import integration_router
+
+_all_ = [
+    "auth",
+    "documentos", 
+    "documentos_versiones",
+    "fuid",
+    "documentos_url", 
+    "integration_router"
+]
